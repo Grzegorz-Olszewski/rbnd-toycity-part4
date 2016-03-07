108ab035da8616fc97b1e9d9990b7b9856c519c7
Grzegorz-Olszewski <grzegorz.olszewski10@gmail.com>
2016-03-07 17:27:08 +0100

    First commit. Everything is working up to 112 line in udacidata_test. Before using table csv, constant and git branches.

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,5 +4,22 @@ require 'faker'
 # fake data for testing purposes
 
 def db_seed
-  # Your code goes here!
+	brand = []
+	products_names = []
+	prices = []
+	database = []
+	array = []
+	10.times do |i|
+		brand.push(Faker::Commerce.department)
+		products_names.push(Faker::Commerce.product_name)
+		prices.push(Faker::Commerce.price)
+		product = Product.new(id:i-1,brand:brand[i],name:products_names[i],price:prices[i])
+		database.push(product)
+	end	
+	CSV.open(@data_path,"wb") do |csv|
+		database.each do |data|
+			csv << [data.id,data.brand,data.name,data.price]
+		end
+	end
+
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,64 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+	def self.create(attributes = nil)
+		data_path = File.dirname(__FILE__) + "/../data/data.csv"
+		CSV.foreach(data_path) do |csv|
+			if csv[2] == attributes[:name]
+				return self.new(id: csv[0], brand: csv[1], name: csv[2], price: csv[3])
+			else
+				object = self.new(attributes)
+				CSV.open(data_path, "a") do |csv|
+					csv << [object.id, object.brand, object.name, object.price]
+				end
+				return object
+			end
+		end
+	end
+
+	def self.all
+		data_path = File.dirname(__FILE__) + "/../data/data.csv"
+		array = []
+		CSV.foreach(data_path, headers:true) do |csv|
+			array.push(self.new(id: csv[0], brand: csv[1], name: csv[2], price: csv[3]))
+		end
+		array
+	end
+
+	def self.first(number = nil)
+		array = []
+		if number.nil?
+			self.all[0]
+		else 	
+			number.times do |i|
+				array.push(self.all[i])
+			end
+			array
+		end
+	end
+	def self.last(number = nil)
+		array = []
+		if number.nil?
+			self.all[self.all.length - 1]
+		else 	
+			number.times do |i|
+				array.push(self.all[self.all.length - 1 - i])
+			end	
+			array
+		end
+	end
+	def self.find(index)
+		self.all[index]
+	end
+	def self.destroy(index)
+		data_path = File.dirname(__FILE__) + "/../data/data.csv"
+		array = CSV.read(data_path)
+		deleted = array.delete_if {|row| row[0].to_i == self.find(index).id}
+		CSV.open(data_path, "wb") do |csv|
+			array.each do |ar|
+				csv << ar
+			end
+		end
+		Product.new(deleted)
+	end
 end
