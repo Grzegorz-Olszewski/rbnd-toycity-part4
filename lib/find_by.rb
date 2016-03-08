43e66ca5ba223180108793c78cbbeec135345c48
Grzegorz-Olszewski <grzegorz.olszewski10@gmail.com>
2016-03-08 16:08:08 +0100

    Method find_by_something works, and I'm proud like hell :)

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -1,6 +1,18 @@
+
 class Module
-  def create_finder_methods(*attributes)
-    # Your code goes here!
-    # Hint: Remember attr_reader and class_eval
-  end
+	def create_finder_methods(*attributes)
+		attributes.each do |attribute|
+			find_by = %Q{
+				def find_by_#{attribute}(argument)
+					CSV.foreach(DATA_PATH,:headers => true) do |row|
+						if row["#{attribute}"] == argument
+							return Product.create(id: row[0],brand: row[1], name: row[2], price: row[3])
+						end
+					end
+				end
+			}
+			class_eval(find_by)	
+		end
+	end
+	create_finder_methods :brand, :name, :price
 end
